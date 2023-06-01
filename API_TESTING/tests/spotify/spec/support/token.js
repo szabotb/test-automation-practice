9fd24bf9484cc354f37a7beb745388478dc823df
Bianka_Szabo-Torok <bianka_szabo-torok@epam.com>
2023-06-01 16:02:03 +0200

    credential change

--- API_TESTING/tests/spotify/spec/support/token.js
+++ API_TESTING/tests/spotify/spec/support/token.js
@@ -1,7 +1,7 @@
 const getToken = (async () => {
 
-    const clientId = '0b1dcb8a65c6468e8fd3b94c57b860c9';
-    const clientSecret = '2cb2045c97cd4f8eb66f3ecba8803c18';
+    const clientId = 'id';
+    const clientSecret = 'secret';
 
     const result = await fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
