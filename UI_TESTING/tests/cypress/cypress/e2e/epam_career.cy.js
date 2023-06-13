d352f43b82f5170dba045d9755ef0bdba675e921
Bianka_Szabo-Torok <bianka_szabo-torok@epam.com>
2023-06-13 17:01:45 +0200

    added test for search

--- UI_TESTING/tests/cypress/cypress/e2e/epam_career.cy.js
+++ UI_TESTING/tests/cypress/cypress/e2e/epam_career.cy.js
@@ -1,14 +1,23 @@
 /// <reference types="Cypress" />
-const { visitUrl, acceptCookie, } = require("../support/page_object/career_page");
+const { visitUrl, acceptCookie, pageLoaded, } = require("../support/page_object/career_page");
 
 describe('Epam.com Career page', () => {
 
     before(() => {
         visitUrl();
+        cy.viewport(1280, 720)
         acceptCookie();
     });
 
-    it('Page load test', async () => {
+    it('Career page should load', async () => {
+        pageLoaded();
+    });
 
+    it('Carrer search should work properly', () => {
+        cy.get("[id='new_form_job_search-location']").click()
+        cy.get("[class='select2-results']").should("be.visible")
+        cy.get("[aria-label='Germany']").should("be.visible")
+        cy.get("[aria-label='Germany']").click()
+        cy.get("[aria-label='Germany'] ul li").its("length").should("equal", 10)
     });
 });
--- UI_TESTING/tests/cypress/cypress/support/page_object/career_page.js
+++ UI_TESTING/tests/cypress/cypress/support/page_object/career_page.js
@@ -2,7 +2,13 @@ const BasePage = require("./base_page");
 
 class CareerPage extends BasePage {
     pageLoaded() {
-        
+        cy.get("li[class='breadcrumbs__item']").last().should("contain.text", "Careers")
+        cy.get("[class='job-search recruiting-search']").should("be.visible")
+        cy.get("[id='new_form_job_search-keyword']").should("be.visible")
+        cy.get("[id='new_form_job_search-location']").should("be.visible")
+        cy.get("[id='new_form_job_search-department']").should("be.visible")
+        cy.get("[type='submit']").should("be.visible")
+        cy.get("[class='job-search__filter-list']").should("be.visible")
     }
 }
 
